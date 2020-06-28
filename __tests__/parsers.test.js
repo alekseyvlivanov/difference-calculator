aca8d28fbff735d79384f8a1b5b323e05807dd06
Aleksey Ivanov <aleksey.vl.ivanov@gmail.com>
2020-06-28 14:10:41 +1000

    refactor: remove console.log(s)

--- __tests__/parsers.test.js
+++ __tests__/parsers.test.js
@@ -4,8 +4,5 @@ test('parseJSON', () => {
   const processObject = process.env.PATH;
   const processJSON = JSON.stringify(processObject);
 
-  console.log(processObject);
-  console.log(processJSON);
-
   expect(parseJSON(processJSON)).toEqual(processObject);
 });
