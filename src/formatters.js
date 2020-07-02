1f66c8bcae726c3a69a170b3f5c354402c24c179
Aleksey Ivanov <aleksey.vl.ivanov@gmail.com>
2020-07-02 17:25:58 +1000

    fix: format curles for nested objects

--- src/formatters.js
+++ src/formatters.js
@@ -2,47 +2,49 @@ import _ from 'lodash';
 
 const padding = 2;
 
-const styleValue = (value, level) => {
-  const indent = ' '.repeat(padding * (level + 1));
+const makeGroupIndent = (level) => ' '.repeat(padding * level);
 
+const styleValue = (value, level) => {
+  const indent = makeGroupIndent(level + 1);
   if (!_.isObject(value)) {
     return value;
   }
 
-  return `  {
+  return `{
 ${Object.entries(value)
-  .map(([key, val]) => `${indent}  ${key}: ${styleValue(val, level + 1)}`)
+  .map(([key, val]) => `${indent}    ${key}: ${styleValue(val, level + 1)}`)
   .join('\n')}
 ${indent}}`;
 };
 
 const stylish = (key, status, value, level) => {
-  const indent = ' '.repeat(level === 1 ? 2 : (level + 1) * 2);
+  const indent = makeGroupIndent(level);
+
   let output = '';
 
   switch (status) {
     case 'unmodified':
-      output += `${indent}  ${key}: ${styleValue(value, level + 1)}`;
+      output += `${indent}  ${key}: ${styleValue(value, level)}`;
       break;
 
     case 'added':
-      output += `${indent}+ ${key}: ${styleValue(value, level + 1)}`;
+      output += `${indent}+ ${key}: ${styleValue(value, level)}`;
       break;
 
     case 'removed':
-      output += `${indent}- ${key}: ${styleValue(value, level + 1)}`;
+      output += `${indent}- ${key}: ${styleValue(value, level)}`;
       break;
 
     case 'modified':
-      output += `${indent}- ${key}: ${styleValue(value.value1, level + 1)}\n`;
-      output += `${indent}+ ${key}: ${styleValue(value.value2, level + 1)}`;
+      output += `${indent}- ${key}: ${styleValue(value.value1, level)}\n`;
+      output += `${indent}+ ${key}: ${styleValue(value.value2, level)}`;
       break;
 
     // children
     default:
       output += `${indent}  ${key}: {\n`;
       output += `${value
-        .map((prop) => stylish(prop.key, prop.status, prop.value, level + 1))
+        .map((prop) => stylish(prop.key, prop.status, prop.value, level + 2))
         .join('\n')}\n`;
       output += `${indent}  }`;
   }
@@ -51,6 +53,7 @@ const stylish = (key, status, value, level) => {
 };
 
 const buildOutput = (contents, format) => {
+  // console.log(JSON.stringify(contents, null, ' ', padding));
   const level = 1;
 
   switch (format) {
@@ -58,9 +61,12 @@ const buildOutput = (contents, format) => {
       return JSON.stringify(contents, null, ' ', padding);
 
     default:
-      return `{\n${contents
-        .map(({ key, status, value }) => stylish(key, status, value, level))
-        .join('\n')}\n}\n`;
+      return `{
+${contents
+  .map(({ key, status, value }) => stylish(key, status, value, level))
+  .join('\n')}
+}
+`;
   }
 };
 
