4b5ad092b87cafc07b788033377480d8cdb696d0
Aleksey Ivanov <aleksey.vl.ivanov@gmail.com>
2020-07-08 10:35:15 +1000

    refactor: unnecessary option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,12 +3,9 @@
 import program from 'commander';
 import genDiff from '../src/index.js';
 
-// TODO - get version from package.json with webpack?
-
 program
   .version('1.2.2')
   .description('Compares two configuration files and shows a difference.')
-  .helpOption('-h, --help', 'output usage information')
   .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filePath1, filePath2) =>
